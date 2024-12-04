05ff6ef19b4218b52ec1dcf21fc2902de6591468
tarun <tarunmalviya320@gmail.com>
2024-12-04 11:33:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Platform for unorganized sector
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-screen bg-gray-800 text-white'>
+        <div className='h-screen flex items-center justify-center text-3xl'>
+      
+          <h2>Helpers Website</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
